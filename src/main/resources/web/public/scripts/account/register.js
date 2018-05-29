c19690d53d833ccce1158dc8bac6bee792f68986
NovaFox161 <novamaday@gmail.com>
2018-05-28 20:10:32 -0500

    registration fixes are just annoying.

--- src/main/resources/web/public/scripts/account/register.js
+++ src/main/resources/web/public/scripts/account/register.js
@@ -1,16 +1,13 @@
-var password = document.getElementById("password");
-var confirm_password = document.getElementById("password-confirm");
-
 function validatePassword() {
+    var password = document.getElementById("password");
+    var confirm_password = document.getElementById("password-confirm");
+
     if (password.value !== confirm_password.value) {
         confirm_password.setCustomValidity("Passwords Don't Match");
     } else {
         confirm_password.setCustomValidity('');
     }
 }
-
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
 
 function onSubmit() {
     var bodyRaw = {
